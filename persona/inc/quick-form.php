3a7543c8e336b7778c698698eb93cf0b7286288f
Eduard <49617189+EduardZorici@users.noreply.github.com>
2022-01-28 15:18:31 +0200

    quick form include added - notfunctional yet

---
+++ persona/inc/quick-form.php
@@ -0,0 +1,10 @@
+<form action="" class="quick-homepage-form">
+    <input required type="text" placeholder="Your Name*" name="visitor_name" id="visitor_name_input" />
+    <input required type="email" placeholder="Your Email Address*" name="visitor_email" id="visitor_email_input" />
+    <textarea type="message" placeholder="Write me something nice" name="visitor_message" id="visitor_message_input" rows="5" cols="30"></textarea>
+    <button type="submit" class="list-link">
+        Send by PED<span class="star">*</span>!
+    </button>
+    <small><span class="star">*</span>PED - Acronym for Pigeon Express
+        Delivery</small>
+</form>
